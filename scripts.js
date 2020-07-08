d09dce3ca8aafe59068b139d0d19aea4e6a23856
arizona-kh <arizonaold@MacBook-Pro-admin.local>
2020-07-08 14:34:34 +0300

    2 switcher types: single button with animation + 2 buttons; syles: animations, card fonts, shadows; note: messed up with syle orders - will fix this

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+/* Solution with 2 separate buttons for List & Grid views 
+
 const listViewButton = document.querySelector('.list-view-button');
 const gridViewButton = document.querySelector('.grid-view-button');
 const list = document.querySelector('ol');
@@ -10,4 +12,18 @@ listViewButton.onclick = function () {
 gridViewButton.onclick = function () {
   list.classList.remove('list-view-filter');
   list.classList.add('grid-view-filter');
+} */
+
+document.querySelectorAll('.grid-list').forEach(button => button.addEventListener('click', toggle));
+const list = document.querySelector('ol');
+
+function toggle() {
+    let btn = this;
+    btn.classList.add('animation');
+    btn.classList.toggle('active',);
+    list.classList.toggle('grid-view-filter');
+    list.classList.toggle('list-view-filter');
+    let newElem = btn.cloneNode(true);
+    btn.parentNode.replaceChild(newElem, btn);
+    newElem.addEventListener('click', toggle);
 }
